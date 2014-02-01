6fe65f2bf0b77a4524ca31d61ab4955b3fa0c3fb
Kai Kontio <kai.kontio@gmail.com>
2014-02-01 15:45:30 +0200

    GitHub prerelease cleanup.
    Upgrade jquery (should probably get rid of it as it is way overkill despite
    being very handy). Bumb version. Add markdown readme and license.

--- Fingerpori.wdgt/Fingerpori.js
+++ Fingerpori.wdgt/Fingerpori.js
@@ -1,17 +1,4 @@
-/*
-    Fingerpori widget - Fetches Fingerpori comic strip
-    from <http://www.hs.fi/>
-    
-    --Mursu 
-
-    Fingerpori widget is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied
-    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-*/
-
 widget.onshow = onshow;
-var prev_url, next_url;
 var base_url = "http://www.hs.fi";
 
 function onshow() {
@@ -23,28 +10,31 @@ function load_comic(strip_url) {
 }
 
 function parse_data(data) {
-    current_img_url = $(data).find("#full-comic > div + div + div > img").attr("src");
-    prev_url = base_url + $(data).find(".prev-cm").attr("href");
-    next_url = base_url + $(data).find(".next-cm").attr("href");    
+    var current_img_url = $(data).find("#full-comic > div + div + div > img").attr("src");
+    var prev_url = base_url + $(data).find(".prev-cm").attr("href");
+    var next_url = base_url + $(data).find(".next-cm").attr("href");    
     
     $("#fingerpori").attr("src", current_img_url);
-
+    
+    var prev_arrow = $("#previous");
+    var next_arrow = $("#next");
+    
     //Check if previous comics exist and hide controls if not
     if (prev_url !== "http://www.hs.fi/fingerpori/") {
-        $("#previous").off("click");
-        $("#previous").on("click", function() { load_comic(prev_url); });
-        $("#previous").show();
+        prev_arrow.off("click");
+        prev_arrow.on("click", function() { load_comic(prev_url); });
+        prev_arrow.show();
     } else {
-        $("#previous").off("click");
-        $("#previous").hide();
+        prev_arrow.off("click");
+        prev_arrow.hide();
     }
     if (next_url !== "http://www.hs.fi/fingerpori/") {
-        $("#next").off("click");
-        $("#next").on("click", function() { load_comic(next_url); });
-        $("#next").show();
+        next_arrow.off("click");
+        next_arrow.on("click", function() { load_comic(next_url); });
+        next_arrow.show();
     } else {
-        $("#next").off("click");
-        $("#next").hide();
+        next_arrow.off("click");
+        next_arrow.hide();
     }
 }
 
---
+++ Fingerpori.wdgt/README.md
@@ -0,0 +1,10 @@
+Fingerpori widget
+=================
+
+### Introduction
+
+Fingerpori widget is an OS X dashboard application, which fetches Fingerpori comic strip from http://www.hs.fi/ 
+
+### Fingerpori
+
+Fingerpori is protected by copyright for Pertti Jarla.
